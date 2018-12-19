9c9db114c78e6eba7c9f6b417f43831b7dd20a18
B Gardner <45779111+braft@users.noreply.github.com>
2018-12-19 03:21:49 -0500

    Create magic.py
    This is the juice in the lemon. I've opted not to waste an extra piece of paper, and added the scale function directly to the route. Keeps things tidy.

---
+++ magic.py
@@ -0,0 +1,22 @@
+from flask import render_template, request
+from app.forms import, InputForm
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/scale', methods=['GET', 'POST'])
+def scale():
+    form = InputForm(request.form)
+    if request.method == 'POST' and form.validate():
+        inquiry = form.inquiry.data
+        x = 0
+        p = 0
+        for i in inquiry:
+            x += ord(i.lower()) - 96
+        y = abs(x) % 10
+        if y == 0:
+            p = 10
+        else:
+            p = y
+        answer = p
+    else:
+        answer = None
+    return render_template("scale.html", form = form, answer = answer)
