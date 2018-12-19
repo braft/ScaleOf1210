5563ea3ab8210492829b9a12ad6cfac9b39def68
B Gardner <45779111+braft@users.noreply.github.com>
2018-12-19 03:31:19 -0500

    Update form.py

--- form.py
+++ form.py
@@ -1,6 +1,4 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import ValidationError, DataRequired
+from wtforms import StringField, Form, validators, DataRequired
 
 class InputForm(Form):
     inquiry = StringField(validators=[DataRequired()])
